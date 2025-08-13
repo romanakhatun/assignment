8631d71fd2c5b6312e24e45ed7c480d0f5157c7e
romanakhatun <romanakhatun2004@gmail.com>
2025-08-13 20:11:39 +0600

    array value check

--- assignment-4/problem1.js
+++ assignment-4/problem1.js
@@ -1,9 +1,15 @@
 function totalFine(fare) {
-  if (typeof fare === "string" || fare <= 0) {
-    return "Invalid";
-  } else {
+  if (typeof fare === "number" && fare > 0) {
     const fine = fare + fare * 0.2 + 30;
     return fine;
+  } else {
+    return "Invalid";
   }
 }
+console.log(totalFine(200));
+console.log(totalFine(0));
+console.log(totalFine(50));
+console.log(totalFine(552));
+console.log(totalFine(-35));
+console.log(totalFine("65"));
 console.log(totalFine("Gorib tai ticket katinai"));
---
+++ assignment-4/problem4.js
@@ -0,0 +1,20 @@
+function isSame(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return "Invalid";
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+console.log(isSame([1, 2, 3], [1, 2, 3]));
+console.log(isSame([34, 5, 7, 9], [34, 5, 7]));
+console.log(isSame([1, undefined, 3], [1, null, 3]));
+console.log(isSame([1, 4, 5], [1, 4, 5]));
+console.log(isSame([1, "4", 4], [1, 4, 4]));
+console.log(isSame([2, 5, 6], 256));
+console.log(isSame({ data: [2, 5, 6] }, [2, 5, 6]));
