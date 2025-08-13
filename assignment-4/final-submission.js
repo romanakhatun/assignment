a4ceb9104a41c7d3123f68696eba2af351d70fbc
romanakhatun <romanakhatun2004@gmail.com>
2025-08-13 21:08:32 +0600

    solve all problems

--- assignment-4/final-submission.js
+++ assignment-4/final-submission.js
@@ -1,9 +1,9 @@
 function totalFine(fare) {
-  if (typeof fare === "string" || fare <= 0) {
-    return "Invalid";
-  } else {
+  if (typeof fare === "number" && fare > 0) {
     const fine = fare + fare * 0.2 + 30;
     return fine;
+  } else {
+    return "Invalid";
   }
 }
 
@@ -29,4 +29,47 @@ function bestTeam(player1, player2) {
   } else {
     return "Tie";
   }
+}
+
+function isSame(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return "Invalid";
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function resultReport(marks) {
+  if (!Array.isArray(marks)) {
+    return "Invalid";
+  }
+
+  let sumMarks = 0;
+  let pass = 0;
+  let fail = 0;
+
+  for (let i = 0; i < marks.length; i++) {
+    sumMarks += marks[i];
+    if (marks[i] >= 40) {
+      pass++;
+    } else {
+      fail++;
+    }
+  }
+
+  let finalScore = marks.length > 0 ? Math.round(sumMarks / marks.length) : 0;
+
+  const result = {
+    finalScore: finalScore,
+    pass: pass,
+    fail: fail,
+  };
+
+  return result;
 }
--- assignment-4/problem5.js
+++ assignment-4/problem5.js
@@ -1,3 +1,34 @@
 function resultReport(marks) {
-  // You have to write your code here
+  if (!Array.isArray(marks)) {
+    return "Invalid";
+  }
+
+  let sumMarks = 0;
+  let pass = 0;
+  let fail = 0;
+
+  for (let i = 0; i < marks.length; i++) {
+    sumMarks += marks[i];
+    if (marks[i] >= 40) {
+      pass++;
+    } else {
+      fail++;
+    }
+  }
+
+  let finalScore = marks.length > 0 ? Math.round(sumMarks / marks.length) : 0;
+
+  const result = {
+    finalScore: finalScore,
+    pass: pass,
+    fail: fail,
+  };
+
+  return result;
 }
+
+console.log(resultReport([]));
+console.log(resultReport([98, 87, 67, 91, 92, 33, 87]));
+console.log(resultReport([99, 87, 67, 12, 87]));
+console.log(resultReport([99]));
+console.log(resultReport(100));
